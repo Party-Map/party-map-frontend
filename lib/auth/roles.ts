c896e28be2e692873043465597d63eb6c8f503bc
Adrian Szell <adrian.szell@scademy.com>
2025-11-23 16:01:48 +0100

    chore (Eslint): Remove eslint ignore from roles enum

--- lib/auth/roles.ts
+++ lib/auth/roles.ts
@@ -1,5 +1,5 @@
 export enum Roles {
-    // eslint-disable-next-line no-unused-vars
+
     PERFORMER_MANAGER_USER = "performer_manager_user",
     PLACE_MANAGER_USER = "place_manager_user",
     EVENT_ORGANIZER_USER = "event_organizer_user",
